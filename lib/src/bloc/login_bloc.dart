6c23ffaff48eb9bcbb291960e6b8234c22493c02
Daniel Alvarez <alvarez17dani@gmail.com>
2020-05-22 18:41:34 +0200

    Checking button if it can submit

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -1,14 +1,18 @@
 import 'dart:async';
+import 'package:rxdart/rxdart.dart';
 import 'package:validacion_formularios/src/bloc/validators.dart';
 
 class LoginBloc with Validators{
 
-  final _emailController = StreamController<String>.broadcast();
-  final _passwordController = StreamController<String>.broadcast();
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
   // Recuperar los datos del Stream
   Stream<String> get emailStream => _emailController.stream.transform(validateEmail);
   Stream<String> get passwordStream => _passwordController.stream.transform(validatePassword);
+
+  Stream<bool> get formValidStream => 
+    CombineLatestStream.combine2(emailStream, passwordStream, (e, p) => true);
 
   // Insertar valores al Stream
   Function(String) get changeEmail => _emailController.sink.add;
